bf07d42fbbba7d50964f817f3aaf3a71c8f20c18
Eli Anderson <elianderson03@gmail.com>
2010-12-01 18:42:14 -0800

    adding price 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101201024240) do
+ActiveRecord::Schema.define(:version => 20101201235607) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20101201024240) do
     t.string   "thumbnail"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "price"
   end
 
   create_table "sessions", :force => true do |t|

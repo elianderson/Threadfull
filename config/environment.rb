e7dc92614db8d932f98519d0add7ae586adb0708
Eli Anderson <elianderson03@gmail.com>
2010-12-08 17:52:58 -0800

    working on it

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+ENV['RAILS_ENV'] ||= 'production'
 
 # Initialize the rails application
 RWDFinal::Application.initialize!

a0421119174458b50636dc61345e0c20d30b2b2f
Eli Anderson <elianderson03@gmail.com>
2010-12-10 18:01:42 -0800

    getting views working

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,11 @@ class Category
   
   CATS = ['Under-50', 'Northwest-Rugged','Cool-Stuff' ] 
   
-  attr_accessor :name
+  attr_accessor :name, :url
   
   def initialize(name)
     @name = name
+    @url = name
   end
   
   def products(amount = 3)
